5edb08cc36efa81b13aee0f07302d031f3fc9275
Kevin Nahinu <54668347+kaleinah@users.noreply.github.com>
2021-09-21 08:43:46 -1000

    yes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ziplist
+
+It works
---
+++ ziplist.js
@@ -0,0 +1,18 @@
+
+function zipList(list1, list2){
+  let list = [];
+  for(let i=0; i<list1.length; i++){
+    list.push(list1[i], list2[i]);
+  }
+  return list;
+}
+function zipListTheSimpleWay(list1, list2){
+  return _.zip(list1, list2);
+}
+
+const test1 =  ['a','b', 'c'];
+const test2 = [1,2,3];
+
+
+console.log(zipList(test1, test2));
+console.log(zipListTheSimpleWay(test1, test2));
