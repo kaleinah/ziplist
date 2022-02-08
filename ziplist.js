f2eb6e37a89c7eafe42ab211608079a002a07a11
Kevin Nahinu <kevinan@hawaii.edu>
2022-02-07 20:59:28 -1000

    update2

--- ziplist.js
+++ ziplist.js
@@ -1,3 +1,7 @@
+/**
+ * Been having issues when I create a new repository for github so I have been using a new one.
+ * Couldn't figure out how to fix this issue
+ */
 function zipList(list1, list2) {
   const list3 = [];
   for (let i = 0; i < list1.length; i++) {
